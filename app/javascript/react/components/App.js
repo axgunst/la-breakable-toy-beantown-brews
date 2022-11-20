bee0286ba10e93d1a75cc7ed7166ae3fc80492ee
ANNA GUNST <axgunst@gmail.com>
2022-11-20 12:41:43 -0500

    landing page is home page

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import HomePage from './HomePage'
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+  <h1>Make It So React</h1>
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={HomePage}/>
+    </Switch>
+  </BrowserRouter>
+  )
 }
 
 export default App
