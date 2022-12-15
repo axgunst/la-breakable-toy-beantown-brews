e4af9f4372e919016e042c1bf95f918563ab2dbc
ANNA GUNST <axgunst@gmail.com>
2022-12-15 00:30:57 -0500

    taproom show top section styled

--- app/javascript/react/components/BeerTile.js
+++ app/javascript/react/components/BeerTile.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const BeerTile = props => {
+const BeerTile = (props) => {
   return (
     <div className="cell medium-6 small-12 large-4">
       <Link to={`/taprooms/taproom_id/beers/${props.id}`}>
-      <div className="beer-logo-image-box">
-        <img src={props.image_url} className="beer-logo-image-tile"/>
-      </div>
+        <div className="beer-index-box">
+          <img src={props.image_url} className="beer-index-tile-logo" />
+        </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-
-export default BeerTile
+export default BeerTile;
