4a1c692441d37dfae89bf739029c84671ea7862f
ANNA GUNST <axgunst@gmail.com>
2023-01-10 20:32:36 -0500

    created additional images tables

---
+++ db/migrate/20230111013158_create_beer_images.rb
@@ -0,0 +1,6 @@
+class CreateBeerImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :beer_images do |t|
+    end
+  end
+end
---
+++ db/migrate/20230111013209_create_taproom_images.rb
@@ -0,0 +1,6 @@
+class CreateTaproomImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :taproom_images do |t|
+    end
+  end
+end
