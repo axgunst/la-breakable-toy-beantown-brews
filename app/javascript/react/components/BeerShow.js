3fd342bfa3c975b8010b9f29471acdb431f1508f
ANNA GUNST <axgunst@gmail.com>
2023-07-27 12:09:17 -0400

    updated brewer to brand

--- app/javascript/react/components/BeerShow.js
+++ app/javascript/react/components/BeerShow.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import BeerReviewTile from "./BeerReviewTile"
-import BeerReviewForm from "./BeerReviewForm"
+import BeerReviewTile from "./BeerReviewTile";
+import BeerReviewForm from "./BeerReviewForm";
 
-const BeerShow = (props) => {  
-  const beerReviewTiles = props.reviews.map((beerReview => {
+const BeerShow = (props) => {
+  const beerReviewTiles = props.reviews.map((beerReview) => {
     return (
-      <BeerReviewTile 
+      <BeerReviewTile
         key={beerReview.id}
         id={beerReview.id}
         name={beerReview.name}
@@ -16,37 +16,34 @@ const BeerShow = (props) => {
         photo={beerReview.photo}
         username={beerReview.username}
       />
-    )
-  }))
+    );
+  });
   return (
     <div>
       <h1 className="beer-show-name">{props.name}</h1>
-      <img src={props.image_url} className="beer-show-image"/>
-     
+      <img src={props.image_url} className="beer-show-image" />
+
       <div className="beer-info">
         <h4>{props.name}</h4>
         <h4>{props.style}</h4>
         <h4>{props.abv}</h4>
-        <h4>{props.brewer}</h4>
+        <h4>{props.brand}</h4>
       </div>
 
       <div>
         <h5>{props.description}</h5>
       </div>
-      
+
       <div>
-       <BeerReviewForm
-        addNewReview={props.addNewReview}
-        reviewButton = {props.reviewButton}
-        // destroyReview = {props.destroyReview}
-      /> 
+        <BeerReviewForm
+          addNewReview={props.addNewReview}
+          reviewButton={props.reviewButton}
+          // destroyReview = {props.destroyReview}
+        />
       </div>
       {beerReviewTiles}
     </div>
-  
-  )
+  );
+};
 
-}
-
-export default BeerShow
-
+export default BeerShow;
