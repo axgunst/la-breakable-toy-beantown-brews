1298db44c36b43e165322780e7b6bd6f9a52333a
ANNA GUNST <axgunst@gmail.com>
2023-07-27 19:05:03 -0400

    added brands to paths

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,25 +1,45 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import HomePage from './HomePage'
-import TaproomIndexContainer from './TaproomIndexContainer.js'
-import TaproomShowContainer from './TaproomShowContainer.js'
-import BeerIndexContainer from './BeerIndexContainer.js'
-import BeerShowContainer from './BeerShowContainer.js'
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import BrandIndexContainer from "./BrandIndexContainer.js";
+import BrandShowContainer from "./BrandShowContainer.js";
+import TaproomIndexContainer from "./TaproomIndexContainer.js";
+import TaproomShowContainer from "./TaproomShowContainer.js";
+import BeerIndexContainer from "./BeerIndexContainer.js";
+import BeerShowContainer from "./BeerShowContainer.js";
 // import NewBeer from './NewBeer.js'
 
 export const App = (props) => {
   return (
-   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/taprooms" component={TaproomIndexContainer}/>
-      <Route exact path="/taprooms/:id" component={TaproomShowContainer}/>
-      <Route exact path="/taprooms/:id/beers" component={BeerIndexContainer}/>
-      <Route exact path="/taprooms/:taproom_id/beers/:id" component={BeerShowContainer}/>
-      {/* <Route exact path="/api/v1/taprooms/:taproom_id/beers/new" component={NewBeer}/> */}
-    </Switch>
-   </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/brands" component={BrandIndexContainer} />
+        <Route exact path="/brands/:id" component={BrandShowContainer} />
+        <Route
+          exact
+          path="/brands/:brand_id/taprooms"
+          component={TaproomIndexContainer}
+        />
+        <Route
+          exact
+          path="/brands/:brand_id/taprooms/:id"
+          component={TaproomShowContainer}
+        />
+        <Route
+          exact
+          path="/brands/:brand_id/beers"
+          component={BeerIndexContainer}
+        />
+        <Route
+          exact
+          path="/brands/:brand_id/beers/:id"
+          component={BeerShowContainer}
+        />
+        {/* <Route exact path="/api/v1/taprooms/:taproom_id/beers/new" component={NewBeer}/> */}
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
